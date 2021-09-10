687ea3779a7dbd49b4b6ebc7ccf15af350d5b55b
Raul Ruiz <raul@barexammasters.com>
2021-09-10 16:34:43 -0700

    Added nullable types.

--- src/LokiConnector.php
+++ src/LokiConnector.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Http;
 
 class LokiConnector
 {
-    public static function Log(string $serverPath, string $username, string $password, array $logTexts)
+    public static function Log(string $serverPath, ?string $username, ?string $password, array $logTexts)
     {
         $http = Http::withBasicAuth(
             $username,
